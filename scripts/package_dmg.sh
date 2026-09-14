ede8b445f6472af23d2ed30de84373db9442d98b
JessePhitidis <jphitidis@gmail.com>
2026-09-14 12:07:16 +0100

    Improved packaging (dmg, exe, tar)

---
+++ scripts/package_dmg.sh
@@ -0,0 +1,10 @@
+create-dmg \
+--volname "BirdWatcher Installer" \
+--window-pos 200 120 \
+--window-size 600 400 \
+--icon-size 100 \
+--icon "BirdWatcher.app" 150 190 \
+--hide-extension "BirdWatcher.app" \
+--app-drop-link 450 190 \
+"BirdWatcher-Installer.dmg" \
+"BirdWatcher.app"
